c27087fd5a5133532c501583dbb1e69cc1ea096e
mustafaprogramming <mustafaghaffar763@gmail.com>
2024-04-18 12:07:35 +0500

    Adding word counter

---
+++ build/js/main.js
@@ -0,0 +1,4 @@
+import chalk from 'chalk';
+console.log(chalk.greenBright(`<<<===========================================================================>>>`));
+console.log(chalk.blueBright.bold(`\n\t\t\tWelcome To Mustafa - Word Counter\t\n`));
+console.log(chalk.greenBright(`<<<===========================================================================>>>`));
---
+++ src/main.ts
@@ -0,0 +1,6 @@
+import inquirer from "inquirer";
+import chalk from 'chalk';
+
+console.log(chalk.greenBright(`<<<===========================================================================>>>`))
+console.log(chalk.blueBright.bold(`\n\t\t\tWelcome To Mustafa - Word Counter\t\n`))
+console.log(chalk.greenBright(`<<<===========================================================================>>>`))
