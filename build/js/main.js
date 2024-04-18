1d45f220fd2c1a09d3f171c5332ecfa5b4ee5d27
mustafaprogramming <mustafaghaffar763@gmail.com>
2024-04-18 12:29:06 +0500

    Adding Word Counter

--- build/js/main.js
+++ build/js/main.js
@@ -1,4 +1,15 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
 import chalk from 'chalk';
 console.log(chalk.greenBright(`<<<===========================================================================>>>`));
 console.log(chalk.blueBright.bold(`\n\t\t\tWelcome To Mustafa - Word Counter\t\n`));
 console.log(chalk.greenBright(`<<<===========================================================================>>>`));
+const answer = await inquirer.prompt([
+    {
+        name: 'sentence',
+        type: "input",
+        message: chalk.rgb(255, 155, 33).bold("Enter the sentence to count the words:"),
+    }
+]);
+const words = answer.sentence.trim().split(" ");
+console.log(chalk.rgb(255, 155, 33)(`Your Sentence Word Count Is: ${chalk.greenBright.bold(words.length)}`));
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,59 @@
+#! /usr/bin/env node
 import inquirer from "inquirer";
 import chalk from 'chalk';
 
 console.log(chalk.greenBright(`<<<===========================================================================>>>`))
 console.log(chalk.blueBright.bold(`\n\t\t\tWelcome To Mustafa - Word Counter\t\n`))
 console.log(chalk.greenBright(`<<<===========================================================================>>>`))
+
+const answer: {
+    sentence:string
+}= await inquirer.prompt([
+    {
+        name:'sentence',
+        type:"input",
+        message: chalk.rgb(255, 155, 33).bold("Enter the sentence to count the words:"),
+    }
+])
+
+const words = answer.sentence.trim().split(" ");
+
+console.log(chalk.rgb(255, 155, 33)(`Your Sentence Word Count Is: ${chalk.greenBright.bold(words.length)}`))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
